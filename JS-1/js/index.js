615d0565abfaf5f5735fa58fc597bf81b12e4492
toufiqulhasan25 <toufiqulhasan25@gmail.com>
2025-10-23 10:04:10 +0600

    created index.html files for different folders and one js file.

---
+++ JS-1/js/index.js
@@ -0,0 +1,50 @@
+// window.alert(' Hello Form external JS');
+console.log('Hello form console JS');
+document.writeln("Hello World"+'<br>');
+
+//data type initialization
+let studentId = 123;
+let studentCgpa = 3.75;
+let studentName = 'shuvo';
+let eS = " ";//empty string
+let b = '123'
+
+//print
+document.writeln('<p style = "color:red">Student Id: '+studentId+'<br></p>');
+document.writeln('<p style = "color:red">Student Id: '+studentCgpa+'<br></p>');
+document.writeln('<p style = "color:red">Student Id: '+studentName+'<br></p>');
+
+//console print
+console.log('Student Id: '+studentId);
+console.log('Student CGPA: '+studentCgpa);
+console.log('Student Id: '+studentName);
+
+//type casting
+//Number
+let cgpa = parseInt(studentCgpa);
+document.writeln(cgpa);
+
+//Integer to float
+let id = parseFloat(studentId)/12;
+document.writeln(id);
+document.writeln(typeof(id));
+document.writeln(id.toFixed(3));
+
+//string to number(integer)
+let bS = parseInt(b);
+document.writeln('string to number: '+typeof(bS));
+document.writeln(bS+"<br><br><br>");
+
+//number to striung
+let d = 987
+let dS = String(d);
+document.writeln('number to string: '+typeof(dS));
+document.writeln(dS+"<br><br>");
+
+//string to float
+let e = '456.258'
+document.writeln(e);
+document.writeln('data type: '+typeof(e)+'<br>');
+let eF = parseFloat(e);
+document.writeln(eF);
+document.writeln('data type: '+typeof(eF));
